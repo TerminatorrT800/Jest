8b99b61994fb4e995e221bdb20da82a0c00e5611
TerminatorrT800 <nikonikolic261@gmail.com>
2025-06-18 14:35:15 +0200

    Improvements

--- main.js
+++ main.js
@@ -1,19 +1,102 @@
-const someOrder = {
-  items: [
-    { name: "banana", price: 9, quantity: 5 },
-    { name: "apple", price: 11, quantity: 2 },
-    { name: "shipping", price: 10, quantity:1, shipping: true },
-  ],
-};
+function ship(shipLength) {
+  let length = shipLength;
+  let hits = 0;
 
-const orderTotal = (order) => {
-  let totalItems = order.items.reduce((prev, curr) => prev + (curr.price * curr.quantity), 0);
-  const shipping = order.items.find(x=> x.shipping)
-  const total = totalItems-shipping.price >50 ? totalItems -shipping.price : totalItems
-  return total;
-};
+  const isSunk = () => hits >= length
+
+  const hit = () => hits++
+
+  const getHits = () => hits
+
+  const getLength = () => length
+
+  return {
+    isSunk, hit, getHits, getLength
+  }
+}
 
 
-const price = orderTotal(someOrder)
-console.log(price)
+function gameBoard(size) {
+  let boardSize = size;
+  let ships = []
+  let missedAttacks = []
+  let occupiedCoords = new Set()
+
+  let boardMap = (boardSize) => {
+    let board = {}
+    for (let i = 0; i < boardSize; i++) {
+      for (let j = 0; j < size; j++) {
+        board[`${i},${j}`] = { ship: null, isHit: null }
+      }
+    }
+    return board;
+  }
+
+  let board = boardMap(boardSize)
+  const placeShip = (startCoord, direction, ship) => {
+    const [x, y] = startCoord.split(',').map(Number)
+
+    if (direction == 'horizontal' && boardSize < ship.getLength() + x || direction == 'vertical' && boardSize < ship.getLength() + y) {
+      console.log('Invalid coordinates or ship lenght!')
+    } else {
+      let shipCoords = new Set()
+      switch (direction) {
+        case 'horizontal':
+          for (let i = 0; i < ship.getLength(); i++) {
+            shipCoords.add(`${x + i},${y}`)
+          }
+          break;
+        case 'vertical':
+          for (let i = 0; i < ship.getLength(); i++) {
+            shipCoords.add(`${x},${y + i}`)
+          }
+          break;
+      }
+      let able = true;
+
+
+      for (const coord of shipCoords) {
+        if (occupiedCoords.has(coord)) {
+          able = false;
+          break;
+        }
+      }
+      if (able == true) {
+        ships.push(ship)
+        shipCoords.forEach(cord => {
+          occupiedCoords.add(cord)
+          board[cord].ship = ship
+        })
+      }
+      else return false
+    }
+  }
+
+  const allShipsSunk = () => ships.every(ship => ship.isSunk());
+
+  const receiveAttack = (coord) => {
+    if (board[coord].isHit != null) return;
+
+    board[coord].isHit = true;
+
+    if (board[coord].ship != null) {
+      board[coord].ship.hit()
+      return 'Hit'
+    }
+    else {
+      missedAttacks.push(coord)
+      return 'Miss'
+    }
+  }
+
+  const getMissedAttacks = () => missedAttacks
+  const getBoard = ()=> board
+
+
+
+  return {
+    placeShip, allShipsSunk, receiveAttack, getMissedAttacks
+  }
+
+}
 
